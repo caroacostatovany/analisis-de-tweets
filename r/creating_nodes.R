8cd521924e853c55b02e52786adbbcb2c2c3cda7
ElenaVillano <villaele14@gmail.com>
2021-05-12 21:11:00 -0500

    versión que tiene los primeros gráficos de redes

--- r/creating_nodes.R
+++ r/creating_nodes.R
@@ -3,6 +3,8 @@ library(tidygraph)
 library(ggraph)
 #library(network)
 rm(list=ls())
+
+# LIMPIEZA
 df <- read.csv('/Volumes/MemoriaEle/HeavyData/tweets_sismo/sismo_cdmx_sin_repeticiones_12052021.csv')
 
 usuarios <- tibble(df$username, df$usuarios_mencionados)
@@ -15,6 +17,9 @@ usuarios <- tibble(u_propietario = rep(df$username, sapply(s, length)),
 
 usuarios$u_mencionado <- str_replace_all(usuarios$u_mencionado, " ", "")
 
+
+
+#################version raw(NO HACER CASO)####################
 head(usuarios)
 
 de_donde <- usuarios %>% 
@@ -59,7 +64,70 @@ nodos_users <-
 
 #red_usuarios <- network(edges, vertex.attr = nodes, matrix.type = "edgelist", ignore.eval = FALSE)
 
-#plot(red_usuarios, vertex.cex = 3)
+
+
+############version FElipe#########
+
+users_chidos <- usuarios %>% as_tbl_graph()
+
+
+vertices_users <- users_chidos %>% 
+  activate(edges) %>% 
+  select(to, from) %>% as_tibble()
+
+# agregar
+vertices_agregados_users <- vertices_users %>% 
+  group_by(to, from) %>% 
+  summarise(pax = n())
+
+vertices_agregados_users %>% 
+  arrange(desc(pax)) %>% 
+  filter(pax>3) %>% 
+  tail()
+
+
+
+# nodos, y agregar estado
+nodos_users <- users_chidos %>% activate(nodes) %>% 
+  as_tibble() 
+
+users_chidos_2 <- tbl_graph(nodes = nodos_users, edges = vertices_agregados_users) 
+users_chidos_2
+
+
+mouse <- users_chidos_2 %>% activate(edges) %>% 
+  select(from, to , pax)
+quantile(pull(mouse, pax), seq(0, 1, 0.1))
+
+corte_pax <- 15
+users_grandes <- users_chidos_2 %>% activate(edges) %>% 
+  filter(pax > corte_pax) %>% 
+  activate(nodes) %>% 
+  filter(!node_is_isolated()) #eliminar nodos que quedan sin conexiones
+
+users_grandes %>% 
+  activate(nodes) %>% 
+  #mutate(color_ca = ifelse(estado == "CA", "CA", "Otros")) %>% 
+  ggraph(layout = 'fr', niter = 2000) + 
+  geom_edge_link(arrow = arrow(length = unit(2, 'mm')), alpha = 0.1, colour="gray") + 
+  geom_node_point(aes(colour='blue'),alpha=0.5) +
+  geom_node_text(aes(label=name),  size=2)+
+  theme_graph(base_family = "sans")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
